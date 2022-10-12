84bc6942a09298f8a81766c720e7d8a1d571f158
Valerii21 <109736919+Valerii21@users.noreply.github.com>
2022-10-12 16:08:13 +0300

    Add files via upload

--- 6/2/6.2.py
+++ 6/2/6.2.py
@@ -1,3 +1,22 @@
-stroka = input().split('O')
-res = max(stroka, key=len)
-print(len(res))
+text = input()
+# для подсчета всех идущих подряд Решек
+count_letter_r = 1
+# для нахождения самой длинной последовательности Решек
+max_count_letter_r = 1
+for letter in range(len(text) - 1):
+    # Cверяем по индексам 2 соседних элемента на Р
+    if text[letter] == text[letter + 1] == 'Р':
+        count_letter_r += 1
+        # Ищем максимальную длину такой последовательности
+        if count_letter_r > max_count_letter_r:
+            max_count_letter_r = count_letter_r
+    else:
+        # Если не равны, обнуляем последовательность Решек, 1 потому что отчет начинается с пары.
+        count_letter_r = 1
+# Проверяем, была ли решка вообще
+if 'Р' not in text:
+    print(0)
+else:
+    print(max_count_letter_r)
+
+
