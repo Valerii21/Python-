93a630390896f0dc67b76792468344f4f8b7fa55
Valerii21 <109736919+Valerii21@users.noreply.github.com>
2022-11-10 12:38:05 +0300

    Add files via upload

--- bot3/1.py
+++ bot3/1.py
@@ -6,5 +6,12 @@ def start(message):
 
     bot.send_game(chat_id=message.chat.id,game_short_name='simplegame')
 
+
+@bot.callback_query_handler(func=lambda callback_query:callback_query.game_short_name == 'simplegame')
+def game(call):
+     
+     bot.answer_callback_query(callback_query_id=call.id, url='https://agar.io/#ffa')
+
+
 if __name__ == "__main__":
     bot.polling()
