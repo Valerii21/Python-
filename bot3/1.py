8bfdc95ff3e78d7b2b8faddf3e15a63e96ad98bd
Valerii21 <109736919+Valerii21@users.noreply.github.com>
2022-11-10 12:40:24 +0300

    Add files via upload

--- bot3/1.py
+++ bot3/1.py
@@ -10,7 +10,7 @@ def start(message):
 @bot.callback_query_handler(func=lambda callback_query:callback_query.game_short_name == 'simplegame')
 def game(call):
      
-     bot.answer_callback_query(callback_query_id=call.id, url='https://agar.io/#ffa')
+     bot.answer_callback_query(callback_query_id=call.id, url='https://yandex.ru/games/app/97835?utm_medium=search&utm_source=yandex&utm_campaign=rus_games_general-igra-bezkav_yandex_search_460.new%7C59207487&utm_term=%D0%B8%D0%B3%D1%80%D0%B0%20%D0%BE%D0%BD%D0%BB%D0%B0%D0%B9%D0%BD#app-id=97835&catalog-session-uid=catalog-ef3860b7-8ab9-5af6-a682-0ac34fa0fdd0-1668073176418-aca0&rtx-reqid=9216465734800721162&pos=%7B%22listType%22%3A%22categorized%22%2C%22tabCategory%22%3A%22common%22%7D')
 
 
 if __name__ == "__main__":
