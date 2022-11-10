b3a7dcf5982169fd65df10bae1ceec56aed9ebf5
Valerii21 <109736919+Valerii21@users.noreply.github.com>
2022-11-10 12:22:03 +0300

    Add files via upload

---
+++ bot3/1.py
@@ -0,0 +1,10 @@
+import telebot
+bot = telebot.TeleBot('5705362473:AAEECXwJP8CM_2vXgH7evFy4abQMs7O2Mn0')
+
+@bot.message_handler(commands=["start"])
+def start(message):
+
+    bot.send_game(chat_id=message.chat.id,game_short_name='My_game')
+
+if __name__ == "__main__":
+    bot.polling()
